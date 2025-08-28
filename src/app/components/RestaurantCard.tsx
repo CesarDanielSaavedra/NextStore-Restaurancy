13c9b16099a85b74a1b35dc72ff7f67ee4d3534b
Daniel Saavedra <cesarcsaavedra@gmail.com>
2025-08-28 12:05:20 -0300

    se concluye la Card de restaurantes y se implementaa ne las diferentes pages

--- src/app/components/RestaurantCard.tsx
+++ src/app/components/RestaurantCard.tsx
@@ -1,8 +1,16 @@
+import {Restaurant} from "@/types";
 
-export default function RestaurantCard() {
+export default function RestaurantCard({restaurant}: {restaurant: Restaurant}) {
     return (
-        <div>
-            <h1>SOY! LA CARD DEL RESTAURANTE</h1>
-        </div>
+        <article key={restaurant.id} className="p-4 border-2 border-gray-400/50 rounded-lg max-w-lg shadow-md shadow-fuchsia-400/20 hover:ring-1 ring-offset-2 ring-offset-transparent ring-fuchsia-400/20 transition-all duration-500 ease-in-out">
+            <h1 className="mb-3 text-xl font-bold">{restaurant.name}</h1>
+            <p className="mb-3 leading-snug opacity-80">{restaurant.description}</p>
+            <img className="h-[300px] w-full object-cover rounded-lg" src={restaurant.image} alt={restaurant.name} />
+            <small className="inline-flex gap-1 w-full mt-2">
+                <span>⭐</span>
+                <span>{restaurant.score}</span>
+                <span className="font-normal opacity-75">({restaurant.ratings})</span>
+            </small>
+        </article>
     );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,10 +10,10 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className="container m-auto grid min-h-screen grid-rows-[auto,1fr,auto] px-4">
-        <header className="text-xl font-bold leading-[3rem]">Restaurancy</header>
+        <header className="mt-4 text-xl font-bold leading-[3rem]">NEXT RESTAURANCY - {new Date().getFullYear()}</header>
         <main className="py-8">{children}</main>
         <footer className="text-center leading-[3rem] opacity-70">
-          © {new Date().getFullYear()} Restaurancy
+          © {new Date().getFullYear()} Next Restaurancy
         </footer>
       </body>
     </html>
